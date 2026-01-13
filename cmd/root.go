e0494837156b52a5e7a4e15446302db3b0993e8e
Dušan Jakub <dusan.jakub@ysoft.com>
2026-01-13 15:44:13 +0100

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func Execute() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	if len(os.Args) < 2 {
+		return errors.New("no subcommand specified")
+	}
+
+	switch os.Args[1] {
+	case "login":
+		return runLogin(os.Args[2:])
+	default:
+		return fmt.Errorf("unknown subcommand: %s", os.Args[1])
+	}
+}
