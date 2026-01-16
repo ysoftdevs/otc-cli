080e3d166809d40d106f5acc94d1f32df7d6f72f
Dušan Jakub <dusan.jakub@ysoft.com>
2026-01-16 18:01:11 +0100

    feat: cce config --output

--- cmd/cce_config.go
+++ cmd/cce_config.go
@@ -14,16 +14,24 @@ var configCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		commonConfig, err := ParseGlobalFlags()
 
+		cceConfigArgs.ClusterName = args[0]
+		cceConfigArgs.CommonConfig = commonConfig
+
 		if err != nil {
 			fmt.Printf("Error parsing global flags: %s\n", err)
 			return
 		}
-		if err := cce.Config(commonConfig, args[0]); err != nil {
+		if err := cce.Config(cceConfigArgs); err != nil {
 			fmt.Printf("Error printing kubeconfig for CCE cluster '%s': %s\n", args[0], err)
 		}
 	},
 }
 
+var cceConfigArgs = cce.ConfigArgs{
+	OutputPath: "",
+}
+
 func init() {
 	cceCmd.AddCommand(configCmd)
+	configCmd.Flags().StringVar(&cceConfigArgs.OutputPath, "output", cceConfigArgs.OutputPath, "Path to write the kubeconfig file. If not specified, prints to stdout.")
 }
--- services/cce/root.go
+++ services/cce/root.go
@@ -52,19 +52,25 @@ func List(commonConfig *config.CommonConfig) error {
 	return nil
 }
 
-func Config(commonConfig *config.CommonConfig, clusterName string) error {
-	cce, err := getCCEClouds(commonConfig)
+type ConfigArgs struct {
+	CommonConfig *config.CommonConfig
+	ClusterName  string
+	OutputPath   string
+}
+
+func Config(args ConfigArgs) error {
+	cce, err := getCCEClouds(args.CommonConfig)
 	if err != nil {
 		return fmt.Errorf("failed to create CCE client: %w", err)
 	}
 
-	clusterList, err := clusters.List(cce, clusters.ListOpts{Name: clusterName})
+	clusterList, err := clusters.List(cce, clusters.ListOpts{Name: args.ClusterName})
 	if err != nil {
 		return fmt.Errorf("failed to list clusters: %w", err)
 	}
 
 	if len(clusterList) == 0 {
-		return fmt.Errorf("cluster '%s' not found", clusterName)
+		return fmt.Errorf("cluster '%s' not found", args.ClusterName)
 	}
 
 	expiryOpts := clusters.ExpirationOpts{
@@ -80,7 +86,15 @@ func Config(commonConfig *config.CommonConfig, clusterName string) error {
 		return fmt.Errorf("unable to marshal cluster kubeconfig: %w", err)
 	}
 
-	fmt.Println(string(configJson))
+	if args.OutputPath != "" {
+		if err := os.WriteFile(args.OutputPath, configJson, 0600); err != nil {
+			return fmt.Errorf("unable to write kubeconfig to file: %w", err)
+		}
+		fmt.Printf("Kubeconfig written to %s\n", args.OutputPath)
+		return nil
+	} else {
+		fmt.Println(string(configJson))
+	}
 
 	return nil
 }
